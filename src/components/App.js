11ab9756f2ff5592fbccf70c052e97ae705141f2
zy-zhangyu <dayuya_zy@163.com>
2024-05-11 09:58:28 +0800

    监听以太坊账户变化
    监听以太坊账户变化

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,19 @@
-// import React from "react";
-// import AutoHideAlert, {alertRef} from "./AutoHideAlert.js";
-// import MintModal, {modalRef} from "./MintModal.js";
-// import ScoreModal, {scoreModalRef} from "./ShowScoreModal.js";
-// import ProSightModal, {proSightModalRef} from "./ProSightModal.js";
-// import {ThemeProvider} from "@mui/material";
-// import {theme} from "../styles/theme.js";
-
-// export const App = () => {
-//     return <ThemeProvider theme={theme}>
-//         <div>
-//             <AutoHideAlert ref={alertRef} />
-//             <MintModal ref={modalRef} />
-//             <ScoreModal ref={scoreModalRef} />
-//             <ProSightModal ref={proSightModalRef} />
-//         </div>
-//     </ThemeProvider>
-// }
-
-import React, { useEffect } from "react";
+import React from "react";
 import AutoHideAlert, { alertRef } from "./AutoHideAlert.js";
 import MintModal, { modalRef } from "./MintModal.js";
 import ScoreModal, { scoreModalRef } from "./ShowScoreModal.js";
 import ProSightModal, { proSightModalRef } from "./ProSightModal.js";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../styles/theme.js";
-import { updateWalletStatus } from "../wallet.js";
 
 export const App = () => {
-    useEffect(() => {
-        const account = web3.eth.accounts[0];
-        const accountInterval = setInterval(() => {
-            if (web3.eth.accounts[0] !== account) {
-                account = web3.eth.accounts[0];
-                updateWalletStatus();
-            }
-        }, 100);
-
-        return () => {
-            clearInterval(accountInterval);
-        };
-    }, []);
-
-    return (
-        <ThemeProvider theme={theme}>
-            <div>
-                <AutoHideAlert ref={alertRef} />
-                <MintModal ref={modalRef} />
-                <ScoreModal ref={scoreModalRef} />
-                <ProSightModal ref={proSightModalRef} />
-            </div>
-        </ThemeProvider>
-    );
-};
+    return <ThemeProvider theme={theme}>
+        <div>
+            <AutoHideAlert ref={alertRef} />
+            <MintModal ref={modalRef} />
+            <ScoreModal ref={scoreModalRef} />
+            <ProSightModal ref={proSightModalRef} />
+        </div>
+    </ThemeProvider>
+}
 
