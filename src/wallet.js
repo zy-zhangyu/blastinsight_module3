70d4893a092fccd1fb69a12c32975a1a5548c5e7
zy-zhangyu <dayuya_zy@163.com>
2024-05-14 10:37:02 +0800

    Update wallet.js
    3

--- src/wallet.js
+++ src/wallet.js
@@ -355,7 +355,7 @@ export const updateWalletStatus = async () => {
 }
 //页面刷新之后就显示小狐狸钱包
 document.addEventListener('DOMContentLoaded', function () {
-    updateConnectButton();
+    // updateConnectButton();
     simulateConnectButtonClick();
     console.log("刷新！！")
 });
